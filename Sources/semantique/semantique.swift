ce79a1e1e526fb39e7d820bb093df5689bd82110
Alban Linard <alban@linard.fr>
2018-02-20 15:25:51 +0100

    Do not use URL, give source as string variables instead.

--- Sources/semantique/semantique.swift
+++ Sources/semantique/semantique.swift
@@ -3,7 +3,9 @@ import AnzenAST
 import Foundation
 
 func parse(what: String) throws -> ModuleDecl {
-    let source = try String(contentsOf: URL(fileURLWithPath: what))
-    let module = try AnzenLib.parse(text: source)
-    return module
+    return try AnzenLib.parse(text: what + "\n")
 }
+
+let example1 = """
+let x = 2
+"""
--- Tests/semantiqueTests/semantiqueTests.swift
+++ Tests/semantiqueTests/semantiqueTests.swift
@@ -2,16 +2,16 @@ import XCTest
 @testable import semantique
 
 class semantiqueTests: XCTestCase {
+
     func test1() throws {
         do {
-            let module = try parse(what: "Examples/ex1.anzen")
+            let module = try parse(what: example1)
             print(module.debugDescription)
         } catch let e {
             print(e)
             throw e
         }
     }
-
 
     static var allTests = [
         ("test1", test1),
