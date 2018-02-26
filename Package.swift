a1db81a70700f98c880ff07e2a0cd8138999eaa9
Alban Linard <alban@linard.fr>
2018-02-26 19:55:28 +0100

    Use dev version of LogicKit to avoid failure on dependencies.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/kyouko-taiga/anzen", .branch("master")),
-        .package(url: "https://github.com/kyouko-taiga/LogicKit", .branch("master")),
+        .package(url: "https://github.com/kyouko-taiga/LogicKit", .branch("dev")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
