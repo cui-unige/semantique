9f6409972c3666ae3e29558c39cd86d94ded67f2
Alban Linard <alban@linard.fr>
2018-03-15 10:06:17 +0100

    Fix dependency on Anzen.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/kyouko-taiga/anzen", .branch("master")),
+        .package(url: "https://github.com/anzen-lang/anzen", .branch("master")),
         .package(url: "https://github.com/kyouko-taiga/LogicKit", .branch("dev")),
     ],
     targets: [
