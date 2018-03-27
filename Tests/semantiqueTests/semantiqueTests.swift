43d5ae82fec18ae342e91fea8a5e106b6248366c
Alban Linard <alban@linard.fr>
2018-03-27 12:01:13 +0200

    Add another test for logic programming.

--- Tests/semantiqueTests/semantiqueTests.swift
+++ Tests/semantiqueTests/semantiqueTests.swift
@@ -30,48 +30,98 @@ class semantiqueTests: XCTestCase {
     //   }
     // }
 
-    func testLogic() throws {
+    // func testLogic() throws {
+    //   // Declare all the literals:
+    //   let zero    = Term.lit("zero")
+    //   let succ    = Term.lit("succ")
+    //   let empty   = Term.lit("empty")
+    //   let add     = Term.lit("add")
+    //   let cons    = Term.lit("cons")
+    //   let node    = Term.lit("node")
+    //   let sumlist = Term.lit("sumlist")
+    //   let sumleft = Term.lit("sumleft")
+    //   // Declaring the literals instead of using strings avoids typos in the terms.
+    //   // Declare the variables:
+    //   let x    : Term = .var("x")
+    //   let y    : Term = .var("y")
+    //   let z    : Term = .var("z")
+    //   let l    : Term = .var("l")
+    //   let r    : Term = .var("r")
+    //   let n    : Term = .var("n")
+    //
+    //   let kb: KnowledgeBase = [
+    //       add[zero, y, y],
+    //       add[x, succ[y], z] => add[succ[x], y, z],
+    //       sumlist[empty, zero],
+    //       (sumlist[l, y] && add[x, y, z]) => sumlist[cons[x, l], z],
+    //       sumleft[empty, zero],
+    //       (sumleft[l, y] && add[n, y, z]) => sumleft[node[n, l, r], z],
+    //   ]
+    //   for question in [
+    //       add[succ[succ[zero]], succ[succ[zero]], x],
+    //       sumlist[cons[succ[zero], empty], x],
+    //       sumleft[node[succ[zero], empty, empty], x],
+    //   ] {
+    //       let answers = kb.ask(question)
+    //       print("Question:", question)
+    //       for answer in answers {
+    //           print("  * ", answer ["x"] ?? "<unknown>")
+    //       }
+    //   }
+    // }
+
+    func testTyping() throws {
       // Declare all the literals:
-      let zero    = Term.lit("zero")
-      let succ    = Term.lit("succ")
-      let empty   = Term.lit("empty")
-      let add     = Term.lit("add")
-      let cons    = Term.lit("cons")
-      let node    = Term.lit("node")
-      let sumlist = Term.lit("sumlist")
-      let sumleft = Term.lit("sumleft")
-      // Declaring the literals instead of using strings avoids typos in the terms.
+      let isInteger = Term.lit("is-integer")
+      let isBoolean = Term.lit("is-boolean")
+      let isString  = Term.lit("is-string")
+      let typeOf    = Term.lit("type-of")
+      let add       = Term.lit("add")
+      let zero      = Term.lit(0)
+      let one       = Term.lit(1)
+      let a         = Term.lit("s:a")
+      let x         = Term.lit("v:x")
+      let y         = Term.lit("v:y")
+      let z         = Term.lit("v:z")
+      let c         = Term.lit("v:c")
       // Declare the variables:
-      let x    : Term = .var("x")
-      let y    : Term = .var("y")
-      let z    : Term = .var("z")
-      let l    : Term = .var("l")
-      let r    : Term = .var("r")
-      let n    : Term = .var("n")
+      let t : Term = .var("t")
+      let u : Term = .var("u")
+      let v : Term = .var("v")
+      let w : Term = .var("w")
+      // let x : Term = .var("x")
+      // let y : Term = .var("y")
 
       let kb: KnowledgeBase = [
-          add[zero, y, y],
-          add[x, succ[y], z] => add[succ[x], y, z],
-          sumlist[empty, zero],
-          (sumlist[l, y] && add[x, y, z]) => sumlist[cons[x, l], z],
-          sumleft[empty, zero],
-          (sumleft[l, y] && add[n, y, z]) => sumleft[node[n, l, r], z],
+        typeOf[zero, isInteger],
+        typeOf[zero, t] => typeOf[x, t],
+
+        typeOf[one, isInteger],
+        typeOf[y, isInteger],
+        typeOf[one, t] => typeOf[y, t],
+
+        typeOf[add[x, y, z], t] => typeOf[z, t],
+
+        // (typeOf[u, t] && typeOf[v, t]) => typeOf[add[u, v, w], t],
+        typeOf[u, t] => typeOf[add[u, v, w], t],
+        typeOf[v, t] => typeOf[add[u, v, w], t],
+
+        typeOf[a, isString],
+        typeOf[add[a, z, c], t] => typeOf[c, t],
       ]
       for question in [
-          add[succ[succ[zero]], succ[succ[zero]], x],
-          sumlist[cons[succ[zero], empty], x],
-          sumleft[node[succ[zero], empty, empty], x],
+          typeOf[c, t],
       ] {
           let answers = kb.ask(question)
           print("Question:", question)
           for answer in answers {
-              print("  * ", answer ["x"] ?? "<unknown>")
+              print("  * ", answer ["t"] ?? "<unknown>")
           }
       }
     }
 
     static var allTests = [
-        ("testLogic", testLogic),
+        ("testTyping", testTyping),
      ]
 
 }
